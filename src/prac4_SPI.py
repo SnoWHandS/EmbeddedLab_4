abd1032083f82705b0294f6604a1d5afe956eeb6
SnoWHandS <dillon.heald@gmail.com>
2018-09-02 10:58:49 +0100

    Added seperate src for SPI

---
+++ src/prac4_SPI.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# Open SPI bus
+spi = spidev.SpiDev()
+# create spi object
+spi.open(0,0)
+# initialise SPI
+
+
+#--------------gets a SPI data--------------
+
+#get CH0
+
+#get CH1
+
+#get CH2
+
+
+
