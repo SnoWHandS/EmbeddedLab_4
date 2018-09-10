d2f898890950621c7b7ab07dc431424152d314d1
Kali Singh <kalium@awful.cloud>
2018-09-10 16:33:12 +0200

    New interrupt handler layout

--- src/prac4_interrupts.py
+++ src/prac4_interrupts.py
@@ -9,7 +9,8 @@ class interruptHandler:
     button2 = 0
     button3 = 0
 
-    def __init__(self, b1, b2, b3):
+    def __init__(self, b1, f1, b2, f2, b3, f3):
+
         """
         Initializer which sets buttons tied to gpio pins and connects them to functions
         """
@@ -24,17 +25,8 @@ class interruptHandler:
         GPIO.setup(self.button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.button3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        GPIO.add_event_detect(self.button1, GPIO.FALLING, callback=b1callback, bouncetime=200)
-        GPIO.add_event_detect(self.button2, GPIO.FALLING, callback=b2callback, bouncetime=200)
-        GPIO.add_event_detect(self.button3, GPIO.FALLING, callback=b3callback, bouncetime=200)
-
-    def b1callback(f):
-        return f
-
-    def b2callback(f):
-        return f
-
-    def b3callback(f):
-        return f
+        GPIO.add_event_detect(self.button1, GPIO.FALLING, callback=f1, bouncetime=200)
+        GPIO.add_event_detect(self.button2, GPIO.FALLING, callback=f2, bouncetime=200)
+        GPIO.add_event_detect(self.button3, GPIO.FALLING, callback=f3, bouncetime=200)
 
 
