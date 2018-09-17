9a2874eb4136eacf5dc7b389b4f918ac9d95aaa4
Kali Singh <kalium@awful.cloud>
2018-09-17 14:40:53 +0200

    add basic functionality

--- src/prac4.py
+++ src/prac4.py
@@ -1,15 +1,34 @@
 import prac4_SPI
 import prac4_interrupts
 import time
+import sys
+
+frequency = 0 # currently selected frequency
+
+flist = [0.5,1,2] # possible frequencies
+
+started = True # are sensors running
+
+timer = 0 # Time since last reset
+
+values = prac4_SPI.values
+
+output = prac4_SPI.output
 
 def reset(port):
-    print("pressed reset"+str(port))
+    timer = 0
+    sys.stdout.write("\u001b[2J")
+    print("reset timer")
+
 
 def freq(port):
-    print("pressed frequency"+str(port))
+    frequency = ((frequency + 1) % 3)
+    print("New frequency: " + str(flist[frequency]))
 
 def stop(port):
-    print("pressed stop"+str(port))
+
+    started = not started
+    print("Set Sensors Running: " + str(started))
 
 def disp(port):
     print("pressed display"+str(port))
@@ -17,4 +36,12 @@ def disp(port):
 handler = prac4_interrupts.interruptHandler(2, reset, 3, freq, 4, stop, 14, disp)
 
 while True:
-    time.sleep(0)
+    time.sleep(flist[frequency])
+    
+    if started:
+        prac4_SPI.updateADCVals()
+        prac4_SPI.formatOutput()
+        
+        print( ' | '.join(output))
+
+
